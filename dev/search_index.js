209702ab3f50eafaca75e6d798e5d9b0da9667bb
Documenter.jl <documenter@juliadocs.github.io>
2022-11-08 14:36:02 +0000

    build based on 9d9cefb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CirculantEmbedding","category":"page"},{"location":"#CirculantEmbedding","page":"Home","title":"CirculantEmbedding","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CirculantEmbedding.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CirculantEmbedding]","category":"page"}]
+}

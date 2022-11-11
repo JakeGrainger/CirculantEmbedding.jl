db53e0731602488bf4c12630bdc275f33baab48a
JakeGrainger <j.p.grainger2@outlook.com>
2022-11-11 15:04:40 +0100

    Add methods for tuples.

--- src/kernels/OscillatoryMatern.jl
+++ src/kernels/OscillatoryMatern.jl
@@ -5,8 +5,10 @@ end
 OscillatoryMatern(σ²,ν,a,offset::SVector{D,T}) where {D,T} = OscillatoryMatern(Matern(σ²,ν,a,Val{D}()), offset)
 OscillatoryMatern(σ²,ν,a,offset::NTuple{D,T}) where {D,T}  = OscillatoryMatern(σ²,ν,a,SVector{D,T}(offset))
 
+Distributions.cov(k::OscillatoryMatern, h::NTuple{D,T}) where {D,T} = cov(k, SVector{D,T}(h))
 Distributions.cov(k::OscillatoryMatern, h::SVector) = cos(2π*(k.offset'*h))*cov(k.matern,h)
 
+sdf(k::OscillatoryMatern, h::NTuple{D,T}) where {D,T} = sdf(k, SVector{D,T}(h))
 function sdf(k::OscillatoryMatern, h::SVector)
     return (sdf(k.matern, h-k.offset)+sdf(k.matern, h+k.offset))/2
 end

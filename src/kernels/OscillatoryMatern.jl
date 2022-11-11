a714b1a69b191c095aac478a027e07170e8667f5
JakeGrainger <j.p.grainger2@outlook.com>
2022-11-11 14:52:03 +0100

    Add missing type parameters.

--- src/kernels/OscillatoryMatern.jl
+++ src/kernels/OscillatoryMatern.jl
@@ -3,7 +3,7 @@ struct OscillatoryMatern{D,P,T,L}
     offset::SVector{D,T}
 end
 OscillatoryMatern(σ²,ν,a,offset::SVector{D,T}) where {D,T} = OscillatoryMatern(Matern(σ²,ν,a,Val{D}()), offset)
-OscillatoryMatern(σ²,ν,a,offset::NTuple{D,T}) = OscillatoryMatern(σ²,ν,a,SVector{D,T}(offset))
+OscillatoryMatern(σ²,ν,a,offset::NTuple{D,T}) where {D,T}  = OscillatoryMatern(σ²,ν,a,SVector{D,T}(offset))
 
 Distributions.cov(k::OscillatoryMatern, h::SVector) = cos(2π*(k.offset'*h))*cov(k.matern,h)
 

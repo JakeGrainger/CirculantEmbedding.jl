2629b910c29f294ca2d44e2235a9bd334875b39a
JakeGrainger <j.p.grainger2@outlook.com>
2022-11-23 11:10:05 +0100

    Fix cov_approximation.

--- src/cov_approximation.jl
+++ src/cov_approximation.jl
@@ -1,12 +1,12 @@
 function approx_cov(Γ::Kernel{D,P}, lags) where {D,P}
     nfreq = choose_nfreq.(lags)
-    Δ = step.(lags)
-    n = length.(lags)
-    freq_f = fftfreq.(nfreq, 1.0./Δ)
+    Δ = step.(lags.iterators)
+    n = length.(lags.iterators)
+    freq = fftfreq.(nfreq, 1.0./Δ)
 
-    f = [aliased_sdf(Γ, (l1,l2)) for l1 in freq_f, l2 in freq_f]
+    f = aliased_sdf.(Γ, Iterators.product(freq...))
     c = FFTView(fft_array(f))
-    return real.(c[CartesianIndices(ntuple(i->-n[i]:n[i]-1,Val{D}()))]) .* (step(freq_f))^2
+    return real.(c[CartesianIndices(ntuple(i->-n[i]:n[i]-1,Val{D}()))]) .* prod(step, freq)
 end
 
 choose_nfreq(lag) = max(length(lag), 2048)

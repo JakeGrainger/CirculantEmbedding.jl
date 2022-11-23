0b4098f2f7c04e6af81539de50e49cdfa29970c2
JakeGrainger <j.p.grainger2@outlook.com>
2022-11-23 13:14:25 +0100

    Frequencies shouldn't have Ref protection.

--- src/cov_approximation.jl
+++ src/cov_approximation.jl
@@ -4,7 +4,7 @@ function approx_cov(Γ::Kernel{D,P}, lags) where {D,P}
     n = length.(lags.iterators)
     freq = fftfreq.(nfreq, 1.0./Δ)
 
-    f = aliased_sdf.(Γ, Ref(Iterators.product(freq...)), Ref(Δ))
+    f = aliased_sdf.(Γ, Iterators.product(freq...), Ref(Δ))
     c = FFTView(fft_array(f))
     return real.(c[CartesianIndices(ntuple(i->-n[i]:n[i]-1,Val{D}()))]) .* prod(step, freq)
 end

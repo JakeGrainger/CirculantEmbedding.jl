b6a44b7c0148a43764156482ce489cf3810a58db
JakeGrainger <j.p.grainger2@outlook.com>
2022-11-23 10:46:31 +0100

    Add covariance approximation.

--- src/CirculantEmbedding.jl
+++ src/CirculantEmbedding.jl
@@ -1,11 +1,12 @@
 module CirculantEmbedding
 
-    using FFTW, StaticArrays, Meshes, SpecialFunctions, Distributions, LinearAlgebra
+    using FFTW, StaticArrays, Meshes, SpecialFunctions, Distributions, LinearAlgebra, FFTViews
     import Base: rand
 
     include("kernels.jl")
     include("simulation.jl")
     include("fft_array.jl")
+    include("cov_approximation.jl")
 
     export GaussianProcess, Matern, OscillatoryMatern, sdf
 end

bfe52839896893bab2b7c16546f6c298cc19dd5d
JakeGrainger <j.p.grainger2@outlook.com>
2022-12-12 12:48:55 +0100

    Add docstrings to kernels.

--- src/kernels.jl
+++ src/kernels.jl
@@ -1,6 +1,11 @@
 abstract type Kernel{D,P} end
 
 Base.broadcastable(k::Kernel) = Ref(k)
+"""
+    var(k::Kernel{D,P}) where {D,P}
+
+Compute the variance of a stationary kernel.
+"""
 Distributions.var(k::Kernel{D,P}) where {D,P} = cov(k, @SVector zeros(D))
 
 struct AdditiveKernel{D,P,K1<:Kernel{D,P},K2<:Kernel{D,P}} <: Kernel{D,P}
@@ -14,6 +19,11 @@ sdf(k::AdditiveKernel, w) = sdf(k.k1, w) + sdf(k.k2, w)
 abstract type KernelSdfOnly{D,P} <: Kernel{D,P} end
 Distributions.cov(k::KernelSdfOnly, h) = error("Covariance not defined for $(typeof(k)). Can still be approximated, see approx_cov.")
 
+"""
+    aliased_sdf(Γ::Kernel{D,P}, k, Δ; K = 3) where {D,P}
+
+Compute the aliased sdf for a given kernel.
+"""
 function aliased_sdf(Γ::Kernel{D,P}, k, Δ; K = 3) where {D,P}
     return sum(sdf(Γ, k .+ j.I ./ Δ) for j in CartesianIndices(ntuple(j->-K:K, Val{D}())))
 end

47123b11d4051a6c9d4baaed8a5a0e1a81279dc0
JakeGrainger <j.p.grainger2@outlook.com>
2023-02-21 15:20:42 +0100

    Should be CartesianIndices.

--- src/randomfield.jl
+++ src/randomfield.jl
@@ -7,7 +7,7 @@ end
 function rand(f::IndependentFields{D,P,T}) where {D,P,T}
     X = rand.(f.fields)
     @assert all(x->size(x)==size(X[1]), X)
-    return [SVector{P,Float64}(X[p][ind] for p in 1:P) for ind in CartesianIndex(X[1])]
+    return [SVector{P,Float64}(X[p][ind] for p in 1:P) for ind in CartesianIndices(X[1])]
 end
 
 Distributions.mean(f::IndependentFields{D,P}) where {D,P} = mean.(f.fields)

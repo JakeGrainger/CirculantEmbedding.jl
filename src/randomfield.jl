46d33747afb1cb13e9a972f7208b42bcf69027bc
JakeGrainger <j.p.grainger2@outlook.com>
2023-02-21 14:28:51 +0100

    Add independent random field.

--- src/randomfield.jl
+++ src/randomfield.jl
@@ -1,1 +1,12 @@
 abstract type RandomField{D,P} end
+
+struct IndependentFields{D,P,T<:NTuple{P,<:RandomField{D,1}}} <: RandomField{D,P}
+    fields::T
+end
+
+rand(f::IndependentFields) = rand.(f.fields)
+
+Distributions.mean(f::IndependentFields{D,P}) where {D,P} = mean.(f.fields)
+Distributions.cov(f::IndependentFields{D,P}, lag) where {D,P} = SMatrix{P,P,Float64,P*P}(i==j ? cov(f.fields[i],lag) : 0.0 for i in 1:P, j in 1:P)
+Distributions.var(f::IndependentFields{D,P}) where {D,P} = SMatrix{P,P,Float64,P*P}(i==j ? var(f.fields[i]) : 0.0 for i in 1:P, j in 1:P)
+sdf(f::IndependentFields{D,P},freq) where {D,P} = SMatrix{P,P,ComplexF16,P*P}(i==j ? sdf(f.fields[i],freq) : 0.0 for i in 1:P, j in 1:P)

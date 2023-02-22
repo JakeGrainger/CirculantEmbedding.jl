7eb2c755ed351689ef5b80f1067bc24d0fa84294
JakeGrainger <j.p.grainger2@outlook.com>
2023-02-22 10:14:58 +0100

    Fix missing type parameter.

--- src/randomfield.jl
+++ src/randomfield.jl
@@ -14,7 +14,7 @@ function rand(f::IndependentFields{D,P,T}) where {D,P,T}
     return [SVector{P,Float64}(X[p][ind] for p in 1:P) for ind in CartesianIndices(X[1])]
 end
 
-Distributions.mean(f::IndependentFields{D,P}) where {D,P} = mean.(f.fields)
-Distributions.cov(f::IndependentFields{D,P}, lag) where {D,P} = SMatrix{P,P,Float64,P*P}(i==j ? cov(f.fields[i],lag) : 0.0 for i in 1:P, j in 1:P)
-Distributions.var(f::IndependentFields{D,P}) where {D,P} = SMatrix{P,P,Float64,P*P}(i==j ? var(f.fields[i]) : 0.0 for i in 1:P, j in 1:P)
-sdf(f::IndependentFields{D,P},freq) where {D,P} = SMatrix{P,P,ComplexF16,P*P}(i==j ? sdf(f.fields[i],freq) : 0.0 for i in 1:P, j in 1:P)
+Distributions.mean(f::IndependentFields{D,P,T}) where {D,P,T} = mean.(f.fields)
+Distributions.cov(f::IndependentFields{D,P,T}, lag) where {D,P,T} = SMatrix{P,P,Float64,P*P}(i==j ? cov(f.fields[i],lag) : 0.0 for i in 1:P, j in 1:P)
+Distributions.var(f::IndependentFields{D,P,T}) where {D,P,T} = SMatrix{P,P,Float64,P*P}(i==j ? var(f.fields[i]) : 0.0 for i in 1:P, j in 1:P)
+sdf(f::IndependentFields{D,P,T},freq) where {D,P} = SMatrix{P,P,ComplexF16,P*P}(i==j ? sdf(f.fields[i],freq) : 0.0 for i in 1:P, j in 1:P)
